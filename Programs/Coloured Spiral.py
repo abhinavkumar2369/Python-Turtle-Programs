69952cc46626511c8156941fde97546c26f5fff5
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-07-18 11:08:15 +0530

    Done ✅

---
+++ Programs/Coloured Spiral.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+colors = ["red", "purple", "blue", "green", "yellow", "orange"]
+my_turtle = turtle.Turtle()
+
+my_turtle.speed(10)
+
+for x in range(360):
+    my_turtle.pencolor(colors[x % 6])
+    my_turtle.width(x / 100 + 1)
+    my_turtle.forward(x)
+    my_turtle.left(59)
+
+turtle.done()
